154d7b2b41c5b19ccea9047913f086e2a7cb8d52
Takashi Binns <takashi.binns@icloud.com>
2023-10-24 13:43:54 -0400

    remove old test class

--- my-tableau-app/pages/index.js
+++ my-tableau-app/pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import CardGrid from '../components/cardgrid';
-import Chart from '../components/test';
 export default function Home({ allPostsData }) { 
   return (
     <Layout home>
       <CardGrid></CardGrid>
-      {/*<Chart></Chart>*/}
     </Layout>
   );
 }
